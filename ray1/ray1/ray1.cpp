acf793e0c3e3971f47d9d7bedd8f3bc0cd833191
jseop <anjs0918@fasoo.com>
2024-12-20 18:00:26 +0900

    U can start in ch6

--- ray1/ray1/ray1.cpp
+++ ray1/ray1/ray1.cpp
@@ -134,7 +134,7 @@ void perlin_spheres()
 {
     hittable_list world;
 
-    shared_ptr<noise_texture> pertext = make_shared<noise_texture>();
+    shared_ptr<noise_texture> pertext = make_shared<noise_texture>(4);
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
     world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
